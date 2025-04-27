30d05cbd44831458cb8ae40a1ec80c78f251eba6
Skylord2705 <shindearyan2705@gmail.com>
2025-04-27 17:37:14 +0530

    JSX Rules

---
+++ JsxRules/src/JSXRules.jsx
@@ -0,0 +1,13 @@
+const JSXRules=()=>{
+    return (
+        <div>
+            <h1>JSX Rules</h1>
+            <p>
+                Jsx must return a single parent element<br/>
+                JSX elements must be properly closed<br/>
+                JSX attributes are written using CamelCase (e.g.,ClassNAme instead of class and htmlfor instead of for)<br/>
+            </p>
+        </div>
+    );
+};
+export default JSXRules;
